e808c30a3dcec75256c88b6d5ed03a8cebf19e62
tuffsteinzwerg <tuffsteinzwerg1@googlemail.com>
2026-05-08 22:28:53 +0200

    Delete assemble.ts

--- assemble.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-const p1 = fs.readFileSync('src/AppNew1.tsx', 'utf8');
-const p2 = fs.readFileSync('src/AppNew2.tsx', 'utf8');
-const p3 = fs.readFileSync('src/AppNew3.tsx', 'utf8');
-const p4 = fs.readFileSync('src/AppNew4.tsx', 'utf8');
-fs.writeFileSync('src/App.tsx', p1 + p2 + p3 + p4);
-console.log("Assembled App.tsx");
