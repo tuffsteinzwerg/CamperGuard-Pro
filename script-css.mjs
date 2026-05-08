eb4aa8517bf8b9c8715afa4832f6a03d2c56ae71
tuffsteinzwerg <tuffsteinzwerg1@googlemail.com>
2026-05-08 22:36:00 +0200

    Delete script-css.mjs

--- script-css.mjs
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-let content = fs.readFileSync('src/index.css', 'utf-8');
-
-content = content.replace(/\.brushed-metal-card\s*\{[\s\S]*?!\s*important;\s*\}/g, '');
-content = content.replace(/\.metal-input\s*\{[\s\S]*?\}\s*\.metal-input:focus\s*\{[\s\S]*?\}/g, '');
-
-fs.writeFileSync('src/index.css', content);
