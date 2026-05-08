5cca195f35628264970813dab8ab214889767b6d
tuffsteinzwerg <tuffsteinzwerg1@googlemail.com>
2026-05-08 22:30:39 +0200

    Delete fix2.cjs

--- fix2.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/App.tsx', 'utf8');
-const errs = code.split('\n').map((l, i) => i + ': ' + l).slice(980, 1020);
-console.log(errs.join('\n'));
