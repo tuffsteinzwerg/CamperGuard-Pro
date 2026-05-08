4561d780e617478cbf87abcc37556b991a64aac2
tuffsteinzwerg <tuffsteinzwerg1@googlemail.com>
2026-05-08 22:39:22 +0200

    Delete script11.js

--- script11.js
+++
@@ -1,7 +0,0 @@
-import { readFileSync, writeFileSync } from 'fs';
-let content = readFileSync('src/App.tsx', 'utf-8');
-
-content = content.replace(/text-\[#888\]/g, 'cg-master-muted');
-
-writeFileSync('src/App.tsx', content);
-console.log('done script11');
