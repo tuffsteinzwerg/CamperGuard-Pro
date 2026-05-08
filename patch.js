d5415cc57e03f8a255374af8f8250e0020c56dcd
tuffsteinzwerg <tuffsteinzwerg1@googlemail.com>
2026-05-08 22:32:14 +0200

    Delete patch.js

--- patch.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-let c = fs.readFileSync('src/App.tsx', 'utf8');
-
-c = c.replace(
-"                                   )}\n                                  </div>\n                              ))}\n                          </div>\n",
-"                                   )}\n                                  </div>\n                                  );\n                              })}\n                          </div>\n");
-
-fs.writeFileSync('src/App.tsx', c);
-console.log("Patched!");
