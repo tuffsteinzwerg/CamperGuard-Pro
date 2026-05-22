4424d3e815e94a1826c030564ff07de85a52687d
tuffsteinzwerg <tuffsteinzwerg1@googlemail.com>
2026-05-22 21:39:09 +0200

    Delete fix.cjs

--- fix.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/views/InhaltView.tsx', 'utf8');
-code = code.replace(/\\n/g, '\n');
-code = code.replace(/\\"/g, '"');
-fs.writeFileSync('src/views/InhaltView.tsx', code);
